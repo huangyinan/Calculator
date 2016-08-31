23b4e4db8229c1a8e1db12c1ace4bf5b225dd103
haishengjulan <haishengjulan@live.cn>
2016-08-31 23:16:34 +0800

    commit test

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,16 +10,96 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var display: UILabel!
+    
+    var userIsInTheMiddleOfTypingANumber = false
+    
+    var allowInputPoint = true
+    
+    @IBAction func operation(sender: UIButton) {
+        let operation = sender.currentTitle!
+        if userIsInTheMiddleOfTypingANumber {
+            enter()
+        }
+        switch operation {
+        case "✖️":
+            performOperation { $0 * $1 }
+        case "➗":
+            if operandStack.last!.isZero {
+        display.text = "+∞"
+            }else {
+                performOperation { $1 / $0 }
+            }
+        case "➕":
+            performOperation { $0 + $1 }
+        case "➖":
+            performOperation { $1 - $0 }
+        case "√":
+            performOperation { sqrt($0) }
+        case "sin":
+            performOperation { sin($0) }
+        case "cos":
+            performOperation { cos($0) }
+        default:
+            break
+        }
+        
+    }
+    
+    func performOperation(operation: (Double,Double) -> Double) {
+        if operandStack.count >= 2 {
+            displayValue = operation(operandStack.removeLast() , operandStack.removeLast())
+            enter()
+        }
+    }
+    
+    private func performOperation(operation: Double -> Double) {
+        if operandStack.count >= 1 {
+            displayValue = operation(operandStack.removeLast())
+            enter()
+        }
+    }
+    
+    @IBAction func appendDigit(sender: UIButton) {
+        let digit = sender.currentTitle!
+        let pai = M_PI
+        print("digit = \(digit)")
+        if userIsInTheMiddleOfTypingANumber {
+            if digit == "." && allowInputPoint {
+                display.text = display.text! + digit
+                allowInputPoint = false
+            }else if digit != "." {
+                display.text = display.text! + digit
+            }else if digit == "π" {
+                //code never go here
+                print("pai = \(pai)")
+                //display.text = NSString(format: "%f" , pai) as String
+                //enter()
+            }
+        } else {
+            display.text = digit
+            userIsInTheMiddleOfTypingANumber = true
+            allowInputPoint = true
+        }
+        
+    }
+    
+    var operandStack = Array<Double>()
+
+    @IBAction func enter() {
+        userIsInTheMiddleOfTypingANumber = false
+        operandStack.append(displayValue)
+        print("operandStack = \(operandStack)")
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    var displayValue: Double {
+        get {
+            return display.text != "π" ? NSNumberFormatter().numberFromString(display.text!)!.doubleValue :M_PI
+        }
+        set {
+            display.text = "\(newValue)"
+            userIsInTheMiddleOfTypingANumber = false
+        }
     }
-
-
 }
 
